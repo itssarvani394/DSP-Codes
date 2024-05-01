757301059cfb5736b3bbbf82828697dfde38e910
Sarvani <129776752+itssarvani394@users.noreply.github.com>
2024-05-02 03:08:17 +0530

    Update Sinusoidal

--- Week-1/Sinusoidal.m
+++ Week-1/Sinusoidal.m
@@ -6,3 +6,15 @@ ts=1/fs
 t=0:ts:0.05
 y=sin(2*pi*f*t)
 plot(t,y)
+
+
+clc
+close all
+clear all
+t=0:0.5:10;
+y=sin(2*pi*100t);
+figure;
+stem(t,y);
+xlabel(“Time”);
+ylabel(“Amplitude”);
+title(“Non-Linear-DT);
