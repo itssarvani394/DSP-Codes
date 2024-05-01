9a8749dff3652bc2533aeb158fe0a57a797272eb
Sarvani <129776752+itssarvani394@users.noreply.github.com>
2024-05-02 03:58:45 +0530

    Update DFT

--- Week-3/DFT.m
+++ Week-3/DFT.m
@@ -1,1 +1,14 @@
-
+clc
+clear all
+close all
+k=0:31
+x=[1 1 1 1 1 zeros(1,27)]
+[h w]=freqz(x,1,"whole")
+plot(w/(2*pi),abs(h))
+grid
+xlabel("Frequency")
+ylabel("Magnitude")
+X=fft(x)
+hold on
+stem(k/32,abs(X),'ro')
+grid on
